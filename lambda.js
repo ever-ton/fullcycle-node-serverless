816650e1282c9117fbf6ca52d01523a3bb271a07
Everton Bezerra <evertonbezerra_93@hotmail.com>
2020-07-08 14:18:26 -0300

    add files to repositorie

---
+++ lambda.js
@@ -0,0 +1,23 @@
+const awsServerlessExpress = require("aws-serverless-express");
+const express = require("express");
+const app = express();
+ 
+app.get("/sum", (req, res) => {
+
+  let a = req.query.a
+  let b = req.query.b
+
+  const result = parseFloat(a) + parseFloat(b)
+
+  res.json({
+    result: result
+  });
+});
+ 
+
+ 
+const server = awsServerlessExpress.createServer(app);
+ 
+exports.handler = (event, context) => {
+  awsServerlessExpress.proxy(server, event, context);
+};
